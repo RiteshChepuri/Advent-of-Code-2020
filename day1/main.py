a6577934fe4d6cad7b8811f62b970656df1e9f58
Ritesh Chepuri <riteshchepuri0@gmail.com>
2024-02-19 22:13:14 +0530

    completed day1

---
+++ day1/main.py
@@ -0,0 +1,29 @@
+from itertools import combinations
+
+with open("1.in") as file:
+    data = [int(line) for line in file]
+
+# Part 1 ===
+for combo in combinations(data, 2):
+    if combo[0] + combo[1] == 2020:
+        part1 = combo[0] * combo[1]
+        break
+
+# Bonus one-liner
+part1 = max(i * j if i + j == 2020 else 0 for i in data for j in data)
+
+
+# Part 2 ===
+for combo in combinations(data, 3):
+    if combo[0] + combo[1] + combo[2] == 2020:
+        part2 = combo[0] * combo[1] * combo[2]
+        break
+
+# Bonus one-liner
+part2 = max(
+    i * j * k if i + j + k == 2020 else 0 for i in data for j in data for k in data
+)
+
+
+print(part1)
+print(part2)
